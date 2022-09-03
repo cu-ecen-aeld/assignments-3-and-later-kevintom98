0e9947b3f934474e04388e45f060465e6235dc29
Kevin <kevintom98@gmail.com>
2022-09-03 14:59:34 -0600

    writer.c completed

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -16,18 +16,17 @@
 ****************************************************************************************/
 
 
-#include <syslog.h> // For syslogging
+#include <syslog.h>     // For syslogging
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>  //For read/write/open file operations
-#include <stdlib.h>  //For exit() function
-#include <stdbool.h> //For using bool type
-
-
-
-
-#include <stdio.h> // CAREFULLLLLLLLL DELETE THISSSSSS
+#include <unistd.h>     //For read/write/open file operations
+#include <stdlib.h>     //For exit() function
+#include <stdbool.h>    //For using bool type
+#include <errno.h>      //For checking error 
+#include <stdio.h>      //For using printf()
+#include <string.h>     //For using string functions such as strlen()
+#include <libgen.h>     //For using basename() function
 
 
 
@@ -36,11 +35,28 @@
 void write_to_file(int fd, char * string)
 {
     ssize_t w_ret;
-    
+    size_t len = strlen(string);
 
-    printf("\n\nSize:%d",sizeof(string));
-    //while(w_ret = write(fd,string,))
-
+    while(len != 0)
+    {
+        w_ret = write (fd,string,len);
+        if(w_ret == -1)
+        {
+            if(errno == EINTR || errno == EAGAIN)
+                continue;
+            else
+            {
+                perror("write");
+                break;
+            }
+        }
+        else
+        {
+            len    -= w_ret;
+            string += w_ret;
+        }
+    }
+    printf("Wrote the string sucessfully to the file!\n");
 }
 
 
@@ -65,20 +81,26 @@ int main(int argc, char *argv[])
 
     if(argc <= 1 || argc > 3)
     {
-        syslog(LOG_ERR,"Need 2 arguments!! \n"
-                       "Arg #1 : Full path of a file (Including filename) \n"
-                       "Arg #2 : Text string to be written to the Arg #1 file \n");
+        syslog(LOG_ERR,"Need 2 arguments!!"
+                       "Arg #1 : Full path of a file (Including filename)"
+                       "Arg #2 : Text string to be written to the Arg #1 file");
 
-        printf("No 2 arguments\n\n");
+        printf("The program needs 2 Arguments \n"
+               "----------------------------"
+               "Arg #1 : Full path of a file (including filename) \n"
+               "Arg #2 : Text string to be written to the Arg #1 file \n");
         exit(1);
     }
 
     char *filepath     = argv[1];
     char *str_to_write = argv[2];
+    char *filename     = basename(argv[1]);
+
+    syslog(LOG_DEBUG,"Writing %s to %s", str_to_write, filename);
+    printf("Writing %s to %s \n", str_to_write, filename);
 
     int fd = open_file(filepath);
     write_to_file(fd,str_to_write);
-
 
     return 0;
 }

882a51e654665ad8146c614cfd45322ce10e52f8
Kevin <kevintom98@gmail.com>
2022-09-04 01:03:36 -0600

    Output files added

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,7 +47,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./a.out "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")

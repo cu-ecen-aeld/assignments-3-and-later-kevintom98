52256aaa1f64dfcb077e40ec6e0c8ddbb93d8f76
Kevin <kevintom98@gmail.com>
2022-09-18 15:25:08 -0600

    finder-test.sh updated

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -51,7 +51,7 @@ do
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-echo $(OUTPUTSTRING) > /tmp/assignment4-result.txt
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
